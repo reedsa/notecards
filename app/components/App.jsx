ff8125c5dd881c6f04f7500c60817a4b4a25254e
Stuart Reed <stuart.reed@colorado.edu>
2016-04-16 19:11:18 -0600

    Implement Alt and Flux

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,58 +1,37 @@
 import React from 'react';
-import uuid from 'node-uuid';
+import AltContainer from 'alt-container';
 
 import Notes from './Notes.jsx';
+import NoteActions from '../actions/NoteActions.jsx';
+import NoteStore from '../stores/NoteStore.js';
 
 export default class App extends React.Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      notes: [
-        {
-          id: uuid.v4(),
-          task: 'Learn Webpack'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Learn React'
-        },
-        {
-          id: uuid.v4(),
-          task: 'Do laundry'
-        }
-      ]
-    };
-  }
-
   render() {
-    const notes = this.state.notes;
 
     return (
       <div>
         <button className="add-note" onClick={this.addNote}>+</button>
-        <Notes
-          notes={notes}
-          onEdit={this.editNote}
-          onDelete={this.deleteNote} />
+
+        <AltContainer
+          stores={[NoteStore]}
+          inject={{
+            notes: () => NoteStore.getState().notes
+          }}
+        >
+          <Notes onEdit={this.editNote} onDelete={this.deleteNote} />
+        </AltContainer>
       </div>
     )
   }
 
   addNote = () => {
-    this.setState({
-      notes: this.state.notes.concat([{
-        id: uuid.v4(),
-        task: 'New task'
-      }])
-    });
+    NoteActions.create({task: 'New Task'})
   };
 
   deleteNote = (id, e) => {
     e.stopPropagation();
 
-    this.setState({
-      notes: this.state.notes.filter(note => note.id !== id)
-    });
+    NoteActions.delete(id);
   };
 
   editNote = (id, task) => {
@@ -61,14 +40,6 @@ export default class App extends React.Component {
       return;
     }
 
-    const notes = this.state.notes.map(note => {
-      if(note.id === id && task) {
-        note.task = task;
-      }
-
-      return note;
-    });
-
-    this.setState({notes});
+    NoteActions.update({id, task});
   };
 }
